7c34745d33ef612343a543be838a500d45f44ca9
Chain <chainz@foxmail.com>
2017-10-29 23:07:32 +0800

    update

---
+++ jianzhioffer/Test01.java
@@ -0,0 +1,102 @@
+package com.chain.jianzhioffer;
+
+import java.util.Scanner;
+
+import org.junit.Test;
+
+/**
+ * 
+ * 在一个二维数组中，不存在重复元素，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * 
+ * 查找
+ * 
+ * @author chain
+ *
+ */
+public class Test01 {
+
+	private int[][] a;
+
+	private int t;
+
+	private boolean res;
+
+	private int px;
+	private int py;
+
+	@Test
+	public void test() {
+		input();
+
+		test1();
+
+		test2();
+	}
+
+	// 利用矩形的性质，缩小范围
+	private void test2() {
+		init();
+		int trows = a.length;
+		int tcols = a[0].length;
+		// 从右上角开始，往左和往下查找，循环往复
+		int row = 0;
+		int col = a[0].length - 1;
+		while (row < trows && col > -1) {
+			// 右上角的元素
+			if (a[row][col] == t) {
+				res = true;
+				px = row;
+				py = col;
+				break;
+			} else if (a[row][col] > t) {
+				col--;
+			} else {
+				row++;
+			}
+		}
+		output();
+	}
+
+	// 线性做法，从头到尾依次遍历比较
+	private void test1() {
+		init();
+		int trows = a.length;
+		int tcols = a[0].length;
+		for (int i = 0; i < trows; i++)
+			for (int j = 0; j < tcols; j++)
+				if (a[i][j] == t) {
+					res = true;
+					px = i;
+					py = j;
+					break;
+				}
+		output();
+	}
+
+	private void init() {
+		res = false;
+		px = -1;
+		py = -1;
+	}
+
+	private void input() {
+		try (Scanner in = new Scanner(System.in)) {
+			int n = in.nextInt();
+			a = new int[n][n];
+			for (int i = 0; i < n; i++)
+				for (int j = 0; j < n; j++)
+					a[i][j] = in.nextInt();
+			t = in.nextInt();
+			init();
+		}
+	}
+
+	private void output() {
+		System.out.println(res);
+		if (res)
+			System.out.println("[" + px + ", " + py + "]");
+	}
+
+}
